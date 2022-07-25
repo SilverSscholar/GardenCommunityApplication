3f4952b5a2c3d6333be8e6fc4fd24106414fc1e4
SilverSscholar <silversscholar@gmail.com>
2022-07-25 16:16:39 -0400

    Adding Controllers

---
+++ Controllers/GardeningClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GardenCommunityApplication.Controllers
+{
+    public class GardeningClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
