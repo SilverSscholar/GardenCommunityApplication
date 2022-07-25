d062cb27d689e1b8793bca459f0d33a82fd13631
SilverSscholar <silversscholar@gmail.com>
2022-07-25 15:26:00 -0400

    Public Enum (Categories)

---
+++ Data/Enum/GardenContestCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace GardenCommunityApplication.Data.Enum
+{
+    public enum GardenContestCategory
+    {
+        Young_Gardeners,
+        Best_Blocks,
+        Little_Lots,
+        Edible_Gardens,
+        General_Gardens
+    }
+}
---
+++ Data/Enum/GardeningClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace GardenCommunityApplication.Data.Enum
+{
+    public enum GardeningClubCategory
+    {
+        Tiny_Terraces,
+        Wild_And_Free,
+        Beginner_Gardens,
+        Homesteading,
+        Floral_Alleys
+    }
+}
